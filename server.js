487e3f867175fcb6c774e0e76f2502dede3df4b7
unknown <brianna.albo@gmail.com>
2023-02-19 16:07:38 -0500

    adds more files/folders, installs dependencies, establishes db connection with sequelize

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const session = require('express-session');
+const exhbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3005;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    // Sets up session store
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+
+  app.use(session(sess));
+
+

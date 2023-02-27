4ab8b037d07b9a40d5763b9f472531e71abc4315
unknown <brianna.albo@gmail.com>
2023-02-27 14:55:57 -0500

    displays all existing posts on the home page. added routing for users to view a single post and any existing comments for that post

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,9 +1,57 @@
 const router = require('express').Router();
+const { Post, Comment, User } = require('../models/');
 
-router.get('/', (req, res) => {
-  res.render('home');
-  return;
+// router.get('/', (req, res) => {
+//   res.render('home');
+//   return;
+// });
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      order: [['createdAt', 'DESC']],
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        }
+      ]
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('home', { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('single-post', { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 router.get('/login', (req, res) => {
   res.render('login');
   return;
